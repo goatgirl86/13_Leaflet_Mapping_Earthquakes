ee88ea75157c2a185b7e812da0ca74dd9060b139
miller.katlin <miller.katlin@gmail.com>
2022-03-19 00:54:54 -0600

    Challenge Updates

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -24,8 +24,8 @@ let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-	center: [40.7, -94.5],
-	zoom: 3,
+	center: [31.653, -6.724],
+	zoom: 2,
 	layers: [streets]
 });
 
@@ -164,7 +164,7 @@ d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/
       // opacity: 1,
       // fillOpacity: 1,
       // fillColor: getColor(feature.properties.mag),
-      color: "magenta",
+      color: "black",
       // radius: getRadius(feature.properties.mag),
       stroke: true,
       weight: 2
@@ -213,27 +213,29 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
   // This function determines the color of the marker based on the magnitude of the earthquake.
   function getColor(magnitude) {
     if (magnitude > 6) {
-      return "purple";
+      return "#8F3A84";
     }
     if (magnitude > 5) {
-      return "brown";
+      return "#46E1D4";
     }
-    if (magnitude < 5) {
-      return "blue";
+    return "#F84698";
     }
-  }
 
   // This function determines the radius of the earthquake marker based on its magnitude.
   // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
   function getRadius(magnitude) {
-    if (magnitude < 5) {
-      return 10;
+    if (magnitude > 6) {
+      return magnitude * 6;
     }
-    else if (magnitude === 5) {
-      return magnitude * 2;
+    if (magnitude > 5) {
+      return magnitude * 4;
     }
-    return magnitude * 4;
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 2;
   }
+
 
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
@@ -253,6 +255,35 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
 
   // Then we add the earthquake layer to our map.
   majorQuakes.addTo(map);
+
+  // Here we create a legend control object.
+  let legend2 = L.control({
+    position: "bottomright"
+  });
+  
+  // Then add all the details for the legend
+  legend2.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+  
+    const magnitudes = [0, 5, 6];
+    const colors = [
+      "#F84698",
+      "#46E1D4",
+      "#8F3A84",
+    ];
+  
+  // Looping through our intervals to generate a label with a colored square for each interval.
+    for (var i = 0; i < magnitudes.length; i++) {
+      console.log(colors[i]);
+      div.innerHTML +=
+        "<i style='background: " + colors[i] + "'></i> " +
+        magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+      }
+      return div;
+    };
+  
+    // Finally, we our legend to the map.
+    legend2.addTo(map);
   
   // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
   
